8a21a97f782f106e49d542113812935e86c01fc1
Nabin Karki Thapa <nabin@outlook.com>
2016-08-08 21:30:31 +1000

    Added NToastNotify

--- sql/1_tables.sql
+++ sql/1_tables.sql
@@ -1,39 +1,45 @@
-DROP SCHEMA IF EXISTS core CASCADE;
-CREATE SCHEMA core;
+    DROP SCHEMA IF EXISTS core CASCADE;
+    CREATE SCHEMA core;
 
-DROP TABLE IF EXISTS core.users;
-CREATE TABLE IF NOT EXISTS core.users (
-	id 					bigserial PRIMARY KEY,
-	user_name			varchar(500) UNIQUE NOT NULL,
-	password    		varchar NOT NULL,
-    full_name           varchar(500),
-    role             	varchar(50)
+    DROP TABLE IF EXISTS core.users;
+    CREATE TABLE IF NOT EXISTS core.users (
+        id 					bigserial PRIMARY KEY,
+        user_name			varchar(500) UNIQUE NOT NULL,
+        password    		varchar NOT NULL,
+        full_name           varchar(500),
+        role             	varchar(50)
+        );
+    --One Admin
+    INSERT INTO core.users (user_name, password, full_name, role) VALUES('admin','bpadmin','Baal Pratibha Admin', 'Admin');
+    --One User
+    INSERT INTO core.users (user_name, password,full_name, role) VALUES('samara','shrestha','Samara Shrestha', 'Contestant');
+
+
+    DROP TABLE IF EXISTS core.contestant_details;
+    CREATE TABLE IF NOT EXISTS core.contestant_details (
+        user_id			        bigint UNIQUE REFERENCES core.users ON DELETE CASCADE,
+        age                     int NOT NULL,
+        suburb                  varchar(150) NOT NULL,
+        parents_name            varchar(500) NOT NULL,
+        contact                 varchar(100) NOT NULL,
+        email                   varchar(100) NOT NULL,
+        performance_video_url   text,
+        about_me                text NOT NULL
+
+        );
+    INSERT INTO core.contestant_details (user_id, age,suburb, parents_name, contact, email, about_me) 
+                                    VALUES(1,12,'Hurstville','Hari Bahadur','0449664244','samara@outlook.com','This is about samara shrestha and who she is');
+
+    DROP TABLE IF EXISTS core.votes;
+    CREATE TABLE IF NOT EXISTS core.votes(
+        id                  bigserial PRIMARY KEY,
+        contestant_id       int REFERENCES core.users ON DELETE CASCADE,
+        voter_name          varchar(250) NOT NULL,
+        voter_email         varchar(250) UNIQUE NOT NULL 
     );
---One Admin
-INSERT INTO core.users (user_name, password, full_name, role) VALUES('admin','bpadmin','Baal Pratibha Admin', 'Admin');
---One User
-INSERT INTO core.users (user_name, password,full_name, role) VALUES('samara','shrestha','Samara Shrestha', 'Contestant');
 
-
-DROP TABLE IF EXISTS core.contestant_details;
-CREATE TABLE IF NOT EXISTS core.contestant_details (
-	user_id			        bigint UNIQUE REFERENCES core.users ON DELETE CASCADE,
-    age                     int NOT NULL,
-    suburb                  varchar(150) NOT NULL,
-    parents_name            varchar(500) NOT NULL,
-    contact                 varchar(100) NOT NULL,
-    email                   varchar(100) NOT NULL,
-    performance_video_url   text,
-    about_me                text NOT NULL
-
-	);
-INSERT INTO core.contestant_details (user_id, age,suburb, parents_name, contact, email, about_me) 
-                                VALUES(1,12,'Hurstville','Hari Bahadur','0449664244','samara@outlook.com','This is about samara shrestha and who she is');
-
-DROP TABLE IF EXISTS core.votes;
-CREATE TABLE IF NOT EXISTS core.votes(
-    id                  bigserial PRIMARY KEY,
-    contestant_id       int REFERENCES core.users ON DELETE CASCADE,
-    voter_name          varchar(250) NOT NULL,
-    voter_email         varchar(250) UNIQUE NOT NULL 
-);
+    DROP TABLE IF EXISTS core.shares;
+    CREATE TABLE IF NOT EXISTS core.shares(
+        id                  bigserial PRIMARY KEY,
+        contestant_id       int REFERENCES core.users ON DELETE CASCADE
+    );
--- sql/2_views & functions.sql
+++ sql/2_views & functions.sql
@@ -1,13 +1,31 @@
-DROP FUNCTION IF EXISTS core.get_total_view(bigint);
+DROP FUNCTION IF EXISTS core.get_total_votes(bigint) CASCADE;
 CREATE OR REPLACE FUNCTION core.get_total_votes(bigint) RETURNS integer as $$
         BEGIN
                 RETURN (SELECT COUNT(*) FROM core.votes WHERE contestant_id = $1);
         END;
 $$ LANGUAGE plpgsql;
 
+DROP FUNCTION IF EXISTS core.get_total_shares(bigint);
+CREATE OR REPLACE FUNCTION core.get_total_shares(bigint) RETURNS integer as $$
+        BEGIN
+                RETURN (SELECT COUNT(*) FROM core.shares WHERE contestant_id = $1);
+        END;
+$$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS core.calc_rank_weight(bigint, bigint);
+CREATE OR REPLACE FUNCTION core.calc_rank_weight(bigint, bigint) RETURNS numeric(100, 2) as $$
+        BEGIN
+                RETURN ($1 * 0.40) + ($2 * 0.60);
+        END;
+$$ LANGUAGE plpgsql;
+
+
+
 
 DROP VIEW IF EXISTS core.contestant_view;
 CREATE OR REPLACE VIEW  core.contestant_view AS (
+	SELECT v.*, core.calc_rank_weight(TotalVotes, TotalShares) FROM 
+(
     SELECT  u.id,
             u.user_name as UserName,
             u.password,
@@ -18,8 +36,10 @@ CREATE OR REPLACE VIEW  core.contestant_view AS (
             cd.contact,
             cd.email,
             cd.about_me as AboutMe,
-            core.get_total_votes(cd.user_id) as TotalVotes
+            core.get_total_votes(u.id) as TotalVotes,
+            core.get_total_shares(u.id) as TotalShares
     FROM core.users as u 
         LEFT OUTER JOIN core.contestant_details cd ON cd.user_id = u.id
         WHERE u.Role = 'Contestant'
+) v
 );
